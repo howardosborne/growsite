6066dc102065608d849f63a73065b1114f2eb8b5
howardosborne <hosborne@hotmail.com>
2025-06-10 12:13:19 +0100

    stuff

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,14 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+let settings = {};
+
+async function getSettings(){
+  href = encodeURIComponent(window.location.href);
+  url = `https://script.google.com/macros/s/AKfycbyNvGeZ-QbGCuxf-POFbp1b_r4qPJ-DTxLqEI7YxvltZkabBHfrDd4OGJJPiGw9-2wn/exec?request=settings&href=${href}`
+  const response = await fetch(url);
+  if(response.status == 200){
+    settings = await response.json();
+  }
+}
