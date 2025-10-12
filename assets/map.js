727bf1a4741ad320fb3dc9ddfb0d13e90ec6661b
howardosborne <howardosborneconsulting@gmail.com>
2025-10-12 11:50:10 +0100

    stuff

--- assets/map.js
+++ assets/map.js
@@ -403,7 +403,18 @@ function loadMap(lat=52.3322,lng=-0.2773,zoom=9){
   return true;
 }
 function loadWaterbodies(){
-  let a = loadMap();
+  map = L.map('map').setView([52.3322,-0.2773], 8);
+  var rel = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri',maxZoom: 13});
+  var img = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
+  var top = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'});
+  var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+  var baseMaps = {
+    "Shaded relief":rel,
+    "Satelite":img,
+    "Topological":top,
+    "OpenStreetMap":osm
+  }
+  layerControl = L.control.layers(baseMaps).addTo(map);
   addLine(`/assets/data/GreatOuse.geojson`,"Great Ouse");
 }
 function loadSwims(){
